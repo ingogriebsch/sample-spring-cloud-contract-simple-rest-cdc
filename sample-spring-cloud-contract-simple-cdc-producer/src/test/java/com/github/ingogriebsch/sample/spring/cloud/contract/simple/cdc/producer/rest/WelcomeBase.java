25e99aaf04c0a3a5002e27e47a92b71d5280d2ef
Ingo Griebsch <ingo.griebsch@gmx.de>
2018-11-29 10:44:30 +0100

    refactor: rework controller test base to use controller from ctx

--- sample-spring-cloud-contract-simple-cdc-producer/src/test/java/com/github/ingogriebsch/sample/spring/cloud/contract/simple/cdc/producer/rest/WelcomeBase.java
+++ sample-spring-cloud-contract-simple-cdc-producer/src/test/java/com/github/ingogriebsch/sample/spring/cloud/contract/simple/cdc/producer/rest/WelcomeBase.java
@@ -1,16 +1,23 @@
 package com.github.ingogriebsch.sample.spring.cloud.contract.simple.cdc.producer.rest;
 
-import static io.restassured.module.mockmvc.RestAssuredMockMvc.standaloneSetup;
-
 import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest(classes = WelcomeController.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = WelcomeController.class, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 public abstract class WelcomeBase {
 
+    @Autowired
+    private WelcomeController welcomeController;
+
     @Before
-    public void setup() {
-        standaloneSetup(new WelcomeController());
+    public void before() {
+        RestAssuredMockMvc.standaloneSetup(welcomeController);
     }
 
 }
