4e1ac324db23c6cd66ed146dc5529a5aae59af64
Ingo Griebsch <ingo.griebsch@gmx.de>
2019-02-04 21:22:49 +0100

    chore: correct package declaration

--- sample-spring-cloud-contract-simple-rest-cdc-producer/src/test/resources/contracts/rest/participant/find_should_return_bad_request_if_query_param_name_is_absent.groovy
+++ sample-spring-cloud-contract-simple-rest-cdc-producer/src/test/resources/contracts/rest/participant/find_should_return_bad_request_if_query_param_name_is_absent.groovy
@@ -7,7 +7,8 @@
  * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and limitations under the License.
  */
-package contracts
+package contracts.rest.participant
+
 import org.springframework.cloud.contract.spec.Contract
 
 Contract.make {
@@ -22,7 +23,7 @@ Contract.make {
             accept(applicationJsonUtf8())
         ]}
     }
-    response {
-        status(400)
+    response { 
+        status(400) 
     }
 }
